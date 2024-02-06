cd7ea744174cf799a57059b970275ac20d3c6845
codaubianca <codau_bianca@yahoo.com>
2024-02-06 13:25:25 +0100

    Registered model classes

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Supplier)
+admin.site.register(models.Product)
+admin.site.register(models.ShoppingCart)
+admin.site.register(models.ShoppingCartItem)
+admin.site.register(models.Order)
+admin.site.register(models.ShippingAddress)
